aa85fb9c3f4ae3dbaa715a34cfb8d22e43203a43
Ahmad Hussain <official.ahmadrandhawa@gmail.com>
2025-02-22 18:10:51 +0500

    add auto reloading for browser during development

--- TweetWebsite/urls.py
+++ TweetWebsite/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('__reload__/', include('django_browser_reload.urls')),
     path('admin/', admin.site.urls),
     path('tweet/', include('tweet.urls'))
 ]
