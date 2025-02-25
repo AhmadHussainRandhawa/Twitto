69ffe0719691910b7e79e52cb1924a695d23a057
Ahmad Hussain <official.ahmadrandhawa@gmail.com>
2025-02-25 20:11:49 +0500

    remove message function from tweet views

--- tweet/views.py
+++ tweet/views.py
@@ -4,11 +4,9 @@ from .models import Tweet
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
-from django.http import HttpResponseForbidden
 
 from django.core.mail import send_mail
 from django.conf import settings
-from django.http import HttpResponse
 
 
 
@@ -30,7 +28,6 @@ def tweetCreate(request):
             tweet = form.save(commit=False) # form.save() here converts the data into Tweet model object.
             tweet.user = request.user
             tweet.save()
-            messages.success(request, "✅ Tweet Created Successfully!")
             return redirect('tweetList')
     else:
         form = TweetForm()  # Empty form for GET request.
@@ -48,7 +45,6 @@ def tweetEdit(request, tweet_id):
             tweet = form.save(commit=False)
             tweet.user = request.user
             tweet.save()
-            messages.success(request, '✅ Tweet Updated Successfully!')
             return redirect('tweetList')
     else: 
         form = TweetForm(instance=tweet)
@@ -59,13 +55,9 @@ def tweetEdit(request, tweet_id):
 @login_required
 def tweetDelete(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id)
-    if tweet.user != request.user:
-        return HttpResponseForbidden("⛔ You are not authorized to delete this tweet.")
-
 
     if request.method == "POST":
         tweet.delete()
-        messages.success(request, '✅ Tweet Deleted Successfully!')
         return redirect('tweetList')
     
     return render(request, 'tweetConfirmDelete.html', {'tweet': tweet})
